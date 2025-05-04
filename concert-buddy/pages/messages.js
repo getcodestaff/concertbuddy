d271e6d43b802d2a7eb31ce65b44f8a3f6ac47dc
getcodestaff <66915102+getcodestaff@users.noreply.github.com>
2025-05-04 12:30:26 -0700

    delete

--- concert-buddy/pages/messages.js
+++
@@ -1,8 +0,0 @@
-export default function Messages() {
-  return (
-    <div className="p-8 text-center">
-      <h2 className="text-2xl font-bold">Messages</h2>
-      <p className="mt-2">(Chat feature coming soon...)</p>
-    </div>
-  );
-}
