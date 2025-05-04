236c1755f70d9709bdd1487633c2f4a4677ef4ef
getcodestaff <66915102+getcodestaff@users.noreply.github.com>
2025-05-04 11:25:03 -0700

    remove app

---
+++ app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Test to see if the app is rendering */}
+      <h1>Welcome to Concert Buddy!</h1>
+
+      {/* Routing to different pages */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
