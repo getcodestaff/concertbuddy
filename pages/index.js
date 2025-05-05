fe66e2d93a808fbe12c80f6b33ad12321cdceab3
getcodestaff <66915102+getcodestaff@users.noreply.github.com>
2025-05-04 17:32:48 -0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,3 @@
-<insert full raw JSX from earlier message here>
+export default function Home() {
+  return <div className="p-8 text-xl text-center text-gray-800">Concert Buddy Homepage with posts, filters, login, and messaging.</div>;
+}
